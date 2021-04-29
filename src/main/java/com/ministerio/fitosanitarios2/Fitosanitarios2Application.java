17c4622ea25dba5ebf6b585901395dcc9302deb1
MarcosVillacanas <marcosvillacanas@icloud.com>
2021-04-29 09:58:40 +0200

    Inicialización del proyecto

---
+++ src/main/java/com/ministerio/fitosanitarios2/Fitosanitarios2Application.java
@@ -0,0 +1,13 @@
+package com.ministerio.fitosanitarios2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fitosanitarios2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fitosanitarios2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ministerio/fitosanitarios2/Fitosanitarios2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ministerio.fitosanitarios2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fitosanitarios2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
