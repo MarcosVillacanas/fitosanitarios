c0da2347081b9ef40df81c30c397930a205007e0
Álvaro Rivas <alvarora12@gmail.com>
2021-05-04 16:53:53 +0200

    Prefijo 'api' en controladores de la API

--- src/main/java/com/ministerio/fitosanitarios2/controladoresREST/PlagaControladorREST.java
+++ src/main/java/com/ministerio/fitosanitarios2/controladoresREST/PlagaControladorREST.java
@@ -7,12 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ministerio.fitosanitarios2.modelos.Plaga;
 import com.ministerio.fitosanitarios2.repositorios.PlagaRepositorio;
 
 @RestController
+@RequestMapping("/api")
 public class PlagaControladorREST {
 
 	@Autowired
--- src/main/java/com/ministerio/fitosanitarios2/controladoresREST/ProductoControladorREST.java
+++ src/main/java/com/ministerio/fitosanitarios2/controladoresREST/ProductoControladorREST.java
@@ -7,12 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ministerio.fitosanitarios2.modelos.Producto;
 import com.ministerio.fitosanitarios2.repositorios.ProductoRepositorio;
 
 @RestController
+@RequestMapping("/api")
 public class ProductoControladorREST {
 
 	@Autowired
